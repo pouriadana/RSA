0e4d3dd2f559cec8087d1f2952b9edb79fb9bfaa
Pouria Dana <pouriadana73@gmail.com>
2024-05-31 10:50:35 +0330

    Prompt for two primes. Calculate n and phi of n

--- src/rsa.cpp
+++ src/rsa.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <format>
 #include <iostream>
 #include <vector>
 
@@ -7,7 +8,19 @@ bool isPrime(int n);
 
 int main()
 {
-    
+    std::cout << "Enter a prime number for p: ";
+    unsigned int p; // must use a bigger type
+    std::cin >> p;
+
+    std::cout << "Enter a prime number for q: ";
+    unsigned int q; // must use a bigger type
+    std::cin >> q;
+
+    unsigned int modulus_n{p * q};
+    std::cout << std::format("n is {}\n", modulus_n);
+
+    unsigned int phi_of_n{(p-1) * (q-1)};
+    std::cout << std::format("totient is {}\n", phi_of_n);
 }
 
 bool isPrime(int n)
