50f56e2d82e6cafb61ffbfb620bfe68a22080f69
Pouria Dana <pouriadana73@gmail.com>
2024-05-31 11:39:47 +0330

    Create candidates for encryption exponent e

--- src/rsa.cpp
+++ src/rsa.cpp
@@ -5,22 +5,38 @@
 
 
 bool isPrime(int n);
+int gcd(int a, int b);
 
 int main()
 {
     std::cout << "Enter a prime number for p: ";
-    unsigned int p; // must use a bigger type
+    int p; // must use a bigger type
     std::cin >> p;
 
     std::cout << "Enter a prime number for q: ";
-    unsigned int q; // must use a bigger type
+    int q; // must use a bigger type
     std::cin >> q;
 
-    unsigned int modulus_n{p * q};
+    int modulus_n{p * q};
     std::cout << std::format("n is {}\n", modulus_n);
 
-    unsigned int phi_of_n{(p-1) * (q-1)};
+    int phi_of_n{(p-1) * (q-1)};
     std::cout << std::format("totient is {}\n", phi_of_n);
+
+    // create a vector of candidates for e (encryption exponent)
+    std::vector<int> e_candidates{};
+    for (int counter{2}; counter < phi_of_n; ++counter)
+    {
+        if (gcd(counter, phi_of_n) == 1)
+        {
+            e_candidates.push_back(counter);
+        }
+    }
+    std::cout << "Candidates for e: ";
+    for (const auto item : e_candidates)
+    {
+        std::cout << item << ' ';
+    }
 }
 
 bool isPrime(int n)
@@ -46,4 +62,16 @@ bool isPrime(int n)
         }
     }
     return primes[n];
+}
+
+int gcd(int a, int b)
+{
+    int r{a % b};
+    while (r)
+    {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    return b;
 }
